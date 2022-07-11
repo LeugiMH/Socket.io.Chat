3db0615c13a31c709698d90622b385e3195cf812
LeugiMH <miguelhenriquegs369@gmail.com>
2022-07-11 20:22:39 -0300

    enviando e recebendo mensagens funcional

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) =>{
+    console.log('Usuário conectado');
+    socket.on('chat message', (msg) => {
+        console.log('mensagem: '+msg)
+        io.emit('chat message', msg);
+    })
+
+    socket.on('disconnect', () => {
+        console.log("Usuário desconectado");
+    });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
